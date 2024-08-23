7ac7d0a2dcc7b568301329b698994e674299d06d
sabrinothman95 <68713978+sabrinothman95@users.noreply.github.com>
2024-08-23 15:13:35 -0400

    generate markdown functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,67 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+function generatelicenceBadge(license) {
+    if (license === "MIT") {
+        return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    } else if (license === "Apache") {
+        return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    } else if (license === "GPL") {
+        return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    } else if (license === "BSD") {
+        return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+    } else {
+        return "";
+    }
+}
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (license === "None") {
+        return "";
+    } else {
+        return generatelicenceBadge(license);
+    }
+
+}
 
 // TODO: Create a function that returns the license link
+function renderLicenseLink(license) {
+    if (license === "MIT") {
+        return "[MIT](https://opensource.org/licenses/MIT)";
+    } else if (license === "Apache") {
+        return "[Apache](https://opensource.org/licenses/Apache-2.0)";
+    } else if (license === "GPL") {
+        return "[GPL](https://www.gnu.org/licenses/gpl-3.0)";
+    } else if (license === "BSD") {
+        return "[BSD](https://opensource.org/licenses/BSD-3-Clause)";
+    } else {
+        return "";
+    }
+}
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    if (license === "None") {
+        return "";
+    } else {
+        return renderLicenseLink(license);
+    }
+}
 
 // TODO: Create a function that returns the license section of README
+function renderLicenseSection(license) {
+    if (license === "None") {
+        return "";
+    } else {
+        return `## License
+        This project is licensed under the ${license} license.`
+    }
+}
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    if (license === "None") {
+        return "";
+    } else {
+        return renderLicenseSection(license);
+    }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
