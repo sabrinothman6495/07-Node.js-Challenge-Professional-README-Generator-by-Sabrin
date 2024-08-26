923e01cf54a646f743119aba2f4b6613303bcf2b
sabrinothman95 <68713978+sabrinothman95@users.noreply.github.com>
2024-08-26 15:58:06 -0400

    trouble shootong code and packages to get a better result

---
+++ index.js
@@ -0,0 +1,74 @@
+// TODO: Include packages needed for this application
+
+import inquirer from "inquirer";
+import fs from "fs";
+import generateMarkdown from "./utils/generateMarkdown";
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide a description of your project.",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the installation instructions for your project?",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What is the usage information for your project?",
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "What are the contribution guidelines for your project?",
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What are the test instructions for your project?",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please choose a license for your project.",
+        choices: ["MIT", "GNU GPLv3", "Apache 2.0", "ISC", "None"],
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+    },
+];
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data);
+
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        writeToFile("README.md", generateMarkdown(data));
+    });
+}
+
+
+
+// Function call to initialize app
+init();
