37d81fb479672a4ecef40c73ecbb61a93eea3dd4
sabrinothman95 <68713978+sabrinothman95@users.noreply.github.com>
2024-08-26 21:48:07 -0400

    fixed issue displaying apache 2.0 license

--- README.md
+++ README.md
@@ -10,8 +10,8 @@
 ## Tests
 
 ## License
-
-
+[ISC](https://opensource.org/license/isc-license-txt)
+[![License](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
 ## Get in contact:
 
 - GitHub: []
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,8 +2,8 @@
 function generatelicenceBadge(license) {
     if (license === "MIT") {
         return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-    } else if (license === "Apache") {
-        return "[![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    } else if (license === "Apache 2.0") {
+        return "[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
     } else if (license === "GPL") {
         return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
     } else if (license === "ISC") {
